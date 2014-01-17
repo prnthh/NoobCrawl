0e98bc526b420506ce37f0c525d2bb9ab0fdc5d3
whatisaroad <pranith_h@yahoo.com.sg>
2014-01-17 21:47:02 +0530

    Added database support. The glitch right now is the fact that the program runs for too long to terminate.

--- scrape.py
+++ scrape.py
@@ -1,7 +1,12 @@
-import urllib, re, sys
+import urllib, re, sys, sqlite3
 
-links = file('sites.txt','wt')
+#links = file('sites.txt','wt')
+#initializing the sql database connection, and the cursor.
+conn = sqlite3.connect('sites.db')
+c = conn.cursor()
 
+
+#this function is recursively called to crawl the site.
 def webCrawl(url, depth):
 	if depth>10:
 		print ("Reached maximum depth here, trying elsewhere \n")
@@ -10,16 +15,27 @@ def webCrawl(url, depth):
 	elif (lambda x: x=="googleapis" or x=="youtube" or x=="gstatic" or x=="google" or x=="google-analytics")(url.split(".")[1]):
 		print ("Found a waste of time. Skipping Reason:2")
 	else:
-		try:
+	#	try:
 			site = urllib.urlopen(url).read()
 			for link in re.findall('''href=["'](.[^"']+)["']''', site, re.I):
 				if(link[0]=="/"):
+					#gotta do something to handle the different cases of / use here
 					link = (url+link)
 				print (link)
-				links.write(link+'\n')
-				webCrawl(link, depth+1)
-		except:
-			print ("You dun goofed")
+#				links.write(link+'\n')
+#This is where I'm testing using an sqlite3 database to check redundancy
+				c.execute("SELECT * FROM sites;")
+				flag = 0
+				for row in c:
+					if(row[0]==link):
+						flag = 1
+						print ("Found a repeated page")
+				if(flag!=1):
+					webCrawl(link, depth+1)
+					c.execute("INSERT INTO sites VALUES ('%s', 1);" % link)
+					print ("I just inserted %s into the table" % link)
+	#	except:
+	#		print ("You dun goofed")
 
 if __name__=="__main__":
 	from sys import argv
